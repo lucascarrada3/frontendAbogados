c3a4fc344b620473c8822bd42f21add2030e724d
luki carrada <luki.carrada@gmail.com>
2025-06-23 13:39:34 -0300

    Url correctas para la autorizacion de railway 2

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,7 +1,20 @@
 // src/utils/socket.ts
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001', {
+
+//local
+
+// const socket = io('http://localhost:3001', {
+//   transports: ['websocket'],
+//   auth: {
+//     token: localStorage.getItem('token'), // Opcional si tenés auth
+//   },
+// });
+
+
+//produccion
+
+const socket = io('https://backendabogados-hsnm.onrender.com', {
   transports: ['websocket'],
   auth: {
     token: localStorage.getItem('token'), // Opcional si tenés auth
